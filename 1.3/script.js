53794b06bd344938e41f0e08169e810d62ac0494
safarlisadiq <95844351+safarlisadiq@users.noreply.github.com>
2022-02-11 10:24:14 +0400

    Add files via upload

---
+++ 1.3/script.js
@@ -0,0 +1,5 @@
+document.getElementById('name').innerHTML= 'Sadiq Seferli';
+document.write('hello world');
+let Inp1 = document.getElementById('inp1').value ;
+let Inp2 = document.getElementById('inp2').value ;
+document.getElementById("btn").innerHTML = Number(Inp1) + Number(Inp2) ; 
